f4f254968ba47e078102b607e1a8a3bd1ceb92b7
Thomas Bodart <thomas.bodart@student-cs.fr>
2025-01-20 18:24:37 +0100

    remodel the custom dataset loader to comply with huggingface

--- mixed_dataset/mixed_dataset.py
+++ mixed_dataset/mixed_dataset.py
@@ -10,29 +10,35 @@ from .data_augmentation import augment_data
 torch.random.manual_seed(1)
 
 
-class MixedDataset(datasets.GeneratorBasedBuilder):
+class MixedDatasetConfig(datasets.BuilderConfig):
     def __init__(
-        self,
-        number_of_train,
-        number_of_test,
-        max_tensor_size=100000,
-        *args,
-        **config_kwargs
+        self, number_of_train, number_of_test, max_tensor_size=100000, **kwargs
     ):
-        super().__init__(*args, **config_kwargs)
+        super().__init__(**kwargs)
         self.number_of_train = number_of_train
         self.number_of_test = number_of_test
         self.max_tensor_size = max_tensor_size
+
+
+class MixedDataset(datasets.GeneratorBasedBuilder):
+    BUILDER_CONFIG_CLASS = MixedDatasetConfig
+
+    def __init__(self, *args, **config_kwargs):
+        super().__init__(*args, **config_kwargs)
 
     def _info(self):
         return datasets.DatasetInfo(
             description="Data augmented dataset for cocktail party problem based on Common Voice dataset from Mozilla",
             features=datasets.Features(
                 {
-                    "audio1": datasets.Array2D((1, self.max_tensor_size), "float32"),
-                    "audio2": datasets.Array2D((1, self.max_tensor_size), "float32"),
+                    "audio1": datasets.Array2D(
+                        (1, self.config.max_tensor_size), "float32"
+                    ),
+                    "audio2": datasets.Array2D(
+                        (1, self.config.max_tensor_size), "float32"
+                    ),
                     "mixed_audio": datasets.Array2D(
-                        (1, self.max_tensor_size), "float32"
+                        (1, self.config.max_tensor_size), "float32"
                     ),
                 }
             ),
@@ -59,11 +65,15 @@ class MixedDataset(datasets.GeneratorBasedBuilder):
 
     def _generate_examples(self, files_path: pathlib.Path, split):
         i = 0
-        break_point = self.number_of_test if split == "test" else self.number_of_train
+        break_point = (
+            self.config.number_of_test
+            if split == "test"
+            else self.config.number_of_train
+        )
         for file1 in files_path:
             track_1 = torchaudio.load(str(file1), format="mp3")[0]
             # Skip if too big
-            if track_1.shape[1] > self.max_tensor_size:
+            if track_1.shape[1] > self.config.max_tensor_size:
                 continue
             for file2 in files_path:
                 if file1 != file2:
@@ -71,14 +81,14 @@ class MixedDataset(datasets.GeneratorBasedBuilder):
                         break
                     track_2 = torchaudio.load(str(file2), format="mp3")[0]
                     # Skip if too big
-                    if track_2.shape[1] > self.max_tensor_size:
+                    if track_2.shape[1] > self.config.max_tensor_size:
                         continue
 
                     track_1 = torch.nn.functional.pad(
-                        track_1, (0, self.max_tensor_size - track_1.shape[1])
+                        track_1, (0, self.config.max_tensor_size - track_1.shape[1])
                     )
                     track_2 = torch.nn.functional.pad(
-                        track_2, (0, self.max_tensor_size - track_2.shape[1])
+                        track_2, (0, self.config.max_tensor_size - track_2.shape[1])
                     )
 
                     ratio = float(torch.rand(1))
