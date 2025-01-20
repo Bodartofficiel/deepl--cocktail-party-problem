8a33ecc1a59fd23bbc6179602ced423c2c21951f
Thomas Bodart <thomas.bodart@student-cs.fr>
2025-01-20 18:04:52 +0100

    name and misc changes

--- src/training.py
+++ src/training.py
@@ -2,16 +2,18 @@ import pathlib
 
 import ffmpeg
 import numpy as np
+import torch.nn.modules as nn
 import torchaudio
 from datasets import load_dataset
 from tqdm import tqdm
 
 number_of_train = 400
 number_of_test = 100
+max_tensor_size = 100000
 
 # path = pathlib.Path("./data/clips")
 
-# iterator = path.glob("*")
+# iterator = list(path.glob("*"))[0]
 # shapes = []
 # for audio_file in tqdm(iterator):
 #     tensor, _ = torchaudio.load(str(audio_file))
@@ -25,9 +27,26 @@ number_of_test = 100
 # # Median shape of the second dimension: 186624
 # exit()
 
+
+## Vizualize the audio source
+# import matplotlib.pyplot as plt
+
+# # Charger le fichier sans normalisation
+# audio, sample_rate = torchaudio.load(str(iterator), normalize=False)
+
+# # Afficher les premiers échantillons
+# plt.plot(audio[0].numpy())  # Seulement les 1000 premiers échantillons
+# plt.title("Valeurs des échantillons audio bruts")
+# plt.xlabel("Échantillons")
+# plt.ylabel("Amplitude")
+# plt.show()
+
+# exit()
+
 dataset = load_dataset(
     path="./mixed_dataset",
     name=f"audio_deepl_{number_of_train}_{number_of_test}",
+    max_tensor_size=max_tensor_size,
     number_of_train=number_of_train,
     number_of_test=number_of_test,
     trust_remote_code=True,
