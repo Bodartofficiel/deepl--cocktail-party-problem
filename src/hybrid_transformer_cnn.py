1b136685711d2e74f7d37ee72d75af186b4c2640
Thomas Bodart <thomas.bodart@student-cs.fr>
2025-01-20 19:16:39 +0100

    first try of model architecture... probably bad but who knows

--- src/hybrid_transformer_cnn.py
+++ src/hybrid_transformer_cnn.py
@@ -1,20 +1,41 @@
+import torch
 import torch.nn.modules as nn
 import torchaudio
+
+short_to = 100000
+device = (
+    "mps"
+    if torch.backends.mps.is_available()
+    else "cuda" if torch.cuda.is_available() else "cpu"
+)
 
 
 class HybridTransformerCNN(nn.Module):
     def __init__(self, input_size, hidden_layer, output_size):
+        super(HybridTransformerCNN, self).__init__()
         self.cnn = nn.Sequential(nn.Conv1d(input_size, hidden_layer, 1), nn.ReLU())
-        self.transformer = nn.Transformer()
-        self.fc = nn.Linear(hidden_layer, output_size)
+        self.transformer = nn.Transformer(d_model=hidden_layer, batch_first=True)
+        self.fc = nn.Linear(hidden_layer, output_size * input_size)
 
-    def forward(self, input):
-        out = self.cnn(input)
-        out = self.transformer(out)
-        out = self.fc(out)
+    def forward(self, input: torch.Tensor):
+        if len(input.shape) == 2:  # No batch
+            input = torch.stack([input])
+
+        out = input.permute(0, 2, 1)
+        out = self.cnn(out)
+        out = out.permute(0, 2, 1)
+        out = self.transformer(out, out)
+        out: torch.Tensor = self.fc(out)
+        out = out.view(out.size(0), -1, input.size(-1))
         return out
 
 
-model = HybridTransformerCNN(100000, 512, 2)
+model = HybridTransformerCNN(short_to, 512, 2).to(device)
 
-audio = torch
+audio = torchaudio.load("data/clips/common_voice_en_41236242.mp3")[0].to(device)
+
+
+audio = audio[:, :short_to]
+# audio = torch.stack(list([audio for i in range(5)]))
+
+output = model(audio)
